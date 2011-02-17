45f84afc452a77a3914db27192464cf1699ba807
Jose Luis Vázquez González <josvazg@gmail.com>
2011-02-17 20:13:41 +0100

    server.go is the server side of the remotization

---
+++ rii/server.go
@@ -0,0 +1,38 @@
+// Copyright 2011 Jose Luis Vázquez González josvazg@gmail.com
+// Use of this source code is governed by a BSD-style
+package rii
+
+import (
+	"os"
+	"io"
+	"rpc"
+	"net"
+	"http"
+)
+
+// Exported Function type
+type FuncHandling func(interface{},[]interface{}) (*[]interface{}, os.Error)
+
+// RII Server using the rpc package as rii transport
+type Server struct {
+	server		*rpc.Server				// rpc server
+	Iface		interface{}				// iface to be invoked
+}
+
+// Create a new RII Server on a local io pipe
+func NewServer(iface interface{}) *Server {
+	s:=&Server{rpc.NewServer(),iface}
+	rpc.Register(s)
+	return s
+}
+
+// Do serve on a (local) pipe
+func (s *Server) ServePipe(rwc io.ReadWriteCloser) {
+	s.server.ServeConn(rwc)
+}
+
+// Do serve on a network listener
+func (s *Server) Serve(l net.Listener) {
+	http.Serve(l,nil)
+}
+
